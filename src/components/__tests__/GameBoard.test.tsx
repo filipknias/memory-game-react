43be8a6d7176515b48e9d9caa72d40ea24b4e1e3
Filip <filip.knias@o2.pl>
2022-09-06 18:58:16 +0200

    correct game board tests

--- src/components/__tests__/GameBoard.test.tsx
+++ src/components/__tests__/GameBoard.test.tsx
@@ -5,7 +5,7 @@ import GameBoard from '../GameBoard/GameBoard';
 describe('Game board component', () => {
   it('renders expected memory items', () => {
     const gridSize = 4;
-    const { getByRole } = render(<GameBoard theme='numbers' players={1} gridSize={gridSize} />);
+    const { getByRole } = render(<GameBoard theme='numbers' gridSize={gridSize} onMoveFinished={() => {}} />);
     const expectedElementsCount = gridSize * gridSize;
     for (let i=0; i<= expectedElementsCount-1; i++) {
       const element = getByRole(`memory-item-${i}`);
