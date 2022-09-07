2b381be9b441412a1a8ebe6b37aec0348d365869
Filip <filip.knias@o2.pl>
2022-09-07 15:12:21 +0200

    game board test corrected

--- src/components/__tests__/GameBoard.test.tsx
+++ src/components/__tests__/GameBoard.test.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import GameBoard from '../GameBoard/GameBoard';
 
 describe('Game board component', () => {
   it('renders expected memory items', () => {
-    const gridSize = 4;
-    const { getByRole } = render(<GameBoard gridSize={gridSize} />);
-    const expectedElementsCount = gridSize * gridSize;
-    for (let i=0; i<= expectedElementsCount-1; i++) {
-      const element = getByRole(`memory-item-${i}`);
-      expect(element).toBeInTheDocument();
-    }
+    waitFor(() => {
+      const gridSize = 6;
+      const { getByRole } = render(<GameBoard gridSize={gridSize} />);
+      const expectedElementsCount = gridSize * gridSize;
+      for (let i=0; i<= expectedElementsCount-1; i++) {
+        const element = getByRole(`memory-item-${i}`);
+        expect(element).toBeInTheDocument();
+      }
+    });
   });
 });
