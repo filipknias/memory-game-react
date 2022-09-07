bc256d62faf116bab7237373b5c1b5c6fc97169f
Filip <filip.knias@o2.pl>
2022-09-07 18:10:01 +0200

    change game board props in tests

--- src/components/__tests__/GameBoard.test.tsx
+++ src/components/__tests__/GameBoard.test.tsx
@@ -6,7 +6,7 @@ describe('Game board component', () => {
   it('renders expected memory items', () => {
     waitFor(() => {
       const gridSize = 6;
-      const { getByRole } = render(<GameBoard gridSize={gridSize} />);
+      const { getByRole } = render(<GameBoard gridSize={gridSize} playersCount={1} />);
       const expectedElementsCount = gridSize * gridSize;
       for (let i=0; i<= expectedElementsCount-1; i++) {
         const element = getByRole(`memory-item-${i}`);
